017fdd0fb5c67caf34427fd79e5c0169e2a1e4f0
Almerra <96740434+Almerra@users.noreply.github.com>
2022-07-18 13:20:57 +0200

    Separated Customs 4th_Customs
    Removed Custom helmets from 4th_Base, moved to 4th_Customs. Moved PAA files with them to new folder.

--- taw4th-community-pack/taw4th_community_pack/4th_Base/config.cpp
+++ taw4th-community-pack/taw4th_community_pack/4th_Base/config.cpp
@@ -77,16 +77,6 @@ class cfgPatches
 			"TAW4th_Equipment_Trooper_Armour_Woodland_Dirt",
 			"TAW4th_Equipment_Trooper_Armour_Woodland_DirtScratch",
 			"TAW4th_Equipment_Trooper_Armour_Woodland_Scratch",
-			
-			"TAW4th_Equipment_Trooper_Helmet_C_Almerra",
-			"TAW4th_Equipment_Trooper_Helmet_C_Laffey",
-			"TAW4th_Equipment_Trooper_Helmet_C_HBTurpin",
-			"TAW4th_Equipment_Trooper_Helmet_C_Dakiiiee",
-			"TAW4th_Equipment_Trooper_Helmet_C_Fluffy",
-			"TAW4th_Equipment_Trooper_Helmet_C_Kaede",
-            "TAW4th_Equipment_Trooper_Helmet_C_Tucc",
-            "TAW4th_Equipment_Trooper_Helmet_C_Bearskylls",
-            "TAW4th_Equipment_Trooper_Helmet_C_Limnades",
 		};
 	};
 };
@@ -1027,98 +1017,6 @@ class CfgWeapons
         hiddenSelectionsTextures[] =
         {
             "taw4th_community_pack\4th_Base\data\base\TAW4th_Equipment_Pilot_Helmet_Co.paa",
-        };
-    };
-
-            //Custom Helmets//
-    class TAW4th_Equipment_Trooper_Helmet_C_Almerra : TAW4th_Equipment_Trooper_Helmet
-    {
-        author = "TAW";
-        scope = 2;
-        displayName = "Clone Trooper P2 Helmet (4th) [Almerra]";
-        hiddenSelectionsTextures[] =
-        {
-            "taw4th_community_pack\4th_Base\data\customs\TAW4th_Equipment_Trooper_Helmet_Almerra_Co.paa",
-        };
-    };
-    class TAW4th_Equipment_Trooper_Helmet_C_Laffey : TAW4th_Equipment_Trooper_Helmet
-    {
-        author = "TAW";
-        scope = 2;
-        displayName = "Clone Trooper P2 Helmet (4th) [Laffey]";
-        hiddenSelectionsTextures[] =
-        {
-            "taw4th_community_pack\4th_Base\data\customs\TAW4th_Equipment_Trooper_Helmet_Laffey_Co.paa",
-        };
-    };
-    class TAW4th_Equipment_Trooper_Helmet_C_HBTurpin : TAW4th_Equipment_Trooper_Helmet
-    {
-        author = "TAW";
-        scope = 2;
-        displayName = "Clone Trooper P2 Helmet (4th) [HBTurpin]";
-        hiddenSelectionsTextures[] =
-        {
-            "taw4th_community_pack\4th_Base\data\customs\TAW4th_Equipment_Trooper_Helmet_HBTurpin_Co.paa",
-        };
-    };
-	class TAW4th_Equipment_Trooper_Helmet_C_Dakiiiee : TAW4th_Equipment_Trooper_Helmet
-    {
-        author = "TAW";
-        scope = 2;
-        displayName = "Clone Trooper P2 Helmet (4th) [Dakiiiee]";
-        hiddenSelectionsTextures[] =
-        {
-            "taw4th_community_pack\4th_Base\data\customs\TAW4th_Equipment_Trooper_Helmet_Dakiiiee_Co.paa",
-        };
-    };
-	class TAW4th_Equipment_Trooper_Helmet_C_Fluffy : TAW4th_Equipment_Trooper_Helmet
-    {
-        author = "TAW";
-        scope = 2;
-        displayName = "Clone Trooper P2 Helmet (4th) [Fluffy]";
-        hiddenSelectionsTextures[] =
-        {
-            "taw4th_community_pack\4th_Base\data\customs\TAW4th_Equipment_Trooper_Helmet_Fluffy_Co.paa",
-        };
-    };
-	class TAW4th_Equipment_Trooper_Helmet_C_Kaede : TAW4th_Equipment_Trooper_Helmet
-    {
-        author = "TAW";
-        scope = 2;
-        displayName = "Clone Trooper P2 Helmet (4th) [Kaede]";
-        hiddenSelectionsTextures[] =
-        {
-            "taw4th_community_pack\4th_Base\data\customs\TAW4th_Equipment_Trooper_Helmet_Kaede_Co.paa",
-        };
-    };
-    class TAW4th_Equipment_Trooper_Helmet_C_BearSkylls : TAW4th_Equipment_Trooper_Helmet
-    {
-        author = "TAW";
-        scope = 2;
-        displayName = "Clone Trooper P2 Helmet (4th) [BearSkylls]";
-        hiddenSelectionsTextures[] =
-        {
-            "taw4th_community_pack\4th_Base\data\customs\TAW4th_Equipment_Trooper_Helmet_BearSkylls_Co.paa",
-        };
-    };
-    class TAW4th_Equipment_Trooper_Helmet_C_Tucc : TAW4th_Equipment_Trooper_Helmet
-    {
-        author = "TAW";
-        scope = 2;
-        displayName = "Clone Trooper P2 Helmet (4th) [Tucc]";
-        hiddenSelectionsTextures[] =
-        {
-            "taw4th_community_pack\4th_Base\data\customs\TAW4th_Equipment_Trooper_Helmet_Tucc_Co.paa",
-        };
-    };
-    class TAW4th_Equipment_Trooper_Helmet_C_Limnades : TAW4th_Equipment_Trooper_Helmet
-    {
-        author = "TAW";
-        scope = 2;
-        displayName = "Clone Trooper P2 Helmet (4th) [Limnades]";
-        hiddenSelectionsTextures[] =
-        {
-            "taw4th_community_pack\4th_Base\data\customs\TAW4th_Equipment_Trooper_Helmet_Limnades_Co.paa",
         };
     };
 };
